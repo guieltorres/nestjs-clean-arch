d1f80ea1d656c4fff7698736b81e454f242aed04
Guilherme Torres <guieltorres@outlook.com>
2025-02-08 15:09:21 -0300

    refactor: update import paths for CreateUserDto and UpdateUserDto for consistency

--- src/users/infra/users.controller.ts
+++ src/users/infra/users.controller.ts
@@ -1,15 +1,15 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './infra/dto/create-user.dto';
-import { UpdateUserDto } from './infra/dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
